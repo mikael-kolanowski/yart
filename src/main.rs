3f0f2c3dc29b8e3f944a33aad3f07cc235fa0f1c
Mikael Kolanowski <mikael.kolanowski97@gmail.com>
2026-01-16 18:20:25 +0100

    Factor out the camera

--- src/main.rs
+++ src/main.rs
@@ -50,16 +50,93 @@ fn ray_color(ray: &Ray, world: &World) -> Color {
     Color::lerp(Color::WHITE, Color::new(0.5, 0.7, 1.0), t)
 }
 
+struct Camera {
+    aspect_ratio: f64,
+    image_width: usize,
+    image_height: usize,
+    center: Point3,
+    pixel_upper_left: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+impl Camera {
+    fn new(aspect_ratio: f64, image_width: usize) -> Self {
+        let image_height = {
+            let w = (image_width as f64 / aspect_ratio) as usize;
+            if w < 1 { 1 } else { w }
+        };
+
+        let center = Point3::ZERO;
+
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (image_width as f64) / (image_height as f64);
+        let camera_center = Point3::ZERO;
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel
+        let pixel_delta_u = viewport_u / image_width as f64;
+        let pixel_delta_v = viewport_v / image_height as f64;
+
+        let viewport_upper_left =
+            camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+
+        let pixel_upper_left = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        Self {
+            aspect_ratio,
+            image_width,
+            image_height,
+            center,
+            pixel_upper_left,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+
+    fn render(&self, world: &World) {
+        eprintln!(
+            "Output image dimensions: {}x{}",
+            self.image_width, self.image_height
+        );
+
+        // Render
+        println!("P3");
+        println!("{} {}", self.image_width, self.image_height);
+        println!("255"); // Max color component
+
+        let mut progress_bar = ProgressBar::new(self.image_height);
+        let rendering_started = Instant::now();
+        for j in 0..self.image_height {
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel_upper_left
+                    + (i as f64 * self.pixel_delta_u)
+                    + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.center;
+                let ray = Ray::new(self.center, ray_direction);
+
+                let pixel_color = ray_color(&ray, &world);
+                pixel_color.write();
+            }
+            progress_bar.increment();
+        }
+        let rendering_finished = Instant::now();
+        progress_bar.finish();
+        eprintln!(
+            "Image rendered in {} ms",
+            (rendering_finished - rendering_started).as_millis()
+        );
+    }
+}
+
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let image_height = {
-        let w = (image_width as f64 / aspect_ratio) as i32;
-        if w < 1 { 1 } else { w }
-    };
-
-    eprintln!("Output image dimensions: {}x{}", image_width, image_height);
 
     // World
     let mut world = World::new();
@@ -73,48 +150,7 @@ fn main() {
         radius: 100.0,
     }));
 
-    // Camera
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f64) / (image_height as f64);
-    let camera_center = Point3::ZERO;
+    let camera = Camera::new(aspect_ratio, image_width);
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    let viewport_upper_left =
-        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-
-    let pixel_upper_left = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-    println!("P3");
-    println!("{} {}", image_width, image_height);
-    println!("255"); // Max color component
-
-    let mut progress_bar = ProgressBar::new(image_height as usize);
-    let rendering_started = Instant::now();
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let pixel_center =
-                pixel_upper_left + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let ray = Ray::new(camera_center, ray_direction);
-
-            let pixel_color = ray_color(&ray, &world);
-            pixel_color.write();
-        }
-        progress_bar.increment();
-    }
-    let rendering_finished = Instant::now();
-    progress_bar.finish();
-    eprintln!(
-        "Image rendered in {} ms",
-        (rendering_finished - rendering_started).as_millis()
-    );
+    camera.render(&world);
 }
