4731cc0676ca19d866e3278200f36545074c5ff4
Mikael Kolanowski <mikael.kolanowski97@gmail.com>
2026-02-25 20:33:08 +0100

    Extract mods from main into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod color;
+pub mod config;
+pub mod math;
+pub mod progressbar;
+pub mod rendering;
+pub mod world;
+
+pub use crate::config::*;
+pub use crate::rendering::*;
+pub use crate::world::World;
